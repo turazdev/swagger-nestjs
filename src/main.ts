eb52881c3219aebd52fe31c00d510b0ce7e9ca10
arturtembe <malunganeartur@gmail.com>
2024-05-12 13:02:15 +0200

    second_update

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-  .setTitle('Documentação com Swagger - Fábrica de Sinapse')
-  .setDescription(
-    'O Swagger (aka OpenApi) é uma biblioteca muito conhecida no universo ba'
-  )
-  .setVersion('1.0')
-  .addTag('users')
-  .build();
+    .setTitle('Documentação com Swagger - Fábrica de Sinapse')
+    .setDescription(
+      'O Swagger (aka OpenApi) é uma biblioteca muito conhecida no universo ba',
+    )
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,19 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
-    @ApiProperty({
-            example: 'Paulo Salvatore',
-            description: `O nome será utilizado para qualquer coisa (Perfil, Home Page)`
-        })
-        name: string;
-        @ApiProperty({
-            example: 'email@email.com',
-            description: `O e-mail é necessário apra o login, mas não necessariamente`
-        })
-        email: string;
-        @ApiProperty({
-            example: '123@abc',
-            description: `É possível conectar com redes sociais sem uma senha, mas parcialmente`
-        })
-        password?: string;
+  @ApiProperty({
+    example: 'Paulo Salvatore',
+    description: `O nome será utilizado para qualquer coisa (Perfil, Home Page)`,
+  })
+  name: string;
+  @ApiProperty({
+    example: 'email@email.com',
+    description: `O e-mail é necessário apra o login, mas não necessariamente`,
+  })
+  email: string;
+  /**
+    * É possível conectar com redes sociais sem uma senha, mas para login usand
+    * é necessário informar uma senha.
+    * @example 123@abc
+  */
+  password?: string;
 }
